6d09d0889ca423d2aa243df9217789d37259c5b5
Iván López <lopez.ivan@gmail.com>
2019-02-21 17:05:23 +0100

    Add travis scripts

---
+++ travis-build-pr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+export EXIT_STATUS=0
+
+echo "Executing tests for branch $TRAVIS_BRANCH"
+./gradlew --console=plain clean
+
+./gradlew --console=plain complete/bookcatalogue:check || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -ne 0 ]]; then
+  exit $EXIT_STATUS
+fi
+
+./gradlew --console=plain complete/bookinventory:check || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -ne 0 ]]; then
+  exit $EXIT_STATUS
+fi
+
+./gradlew --console=plain complete/bookrecommendation:check || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
