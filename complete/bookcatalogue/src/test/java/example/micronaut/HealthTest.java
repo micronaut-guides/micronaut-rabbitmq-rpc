65612b947d65b9481507ee65342568cbd75920fc
Sergio del Amo <sergio.delamo@softamo.com>
2019-03-24 17:38:01 +0100

    Health Tests

---
+++ complete/bookcatalogue/src/test/java/example/micronaut/HealthTest.java
@@ -0,0 +1,46 @@
+package example.micronaut;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HealthTest {
+    private static EmbeddedServer server;
+    private static HttpClient client;
+
+    @BeforeAll
+    public static void setupServer() {
+        server = ApplicationContext.run(EmbeddedServer.class);
+        client = server
+                .getApplicationContext()
+                .createBean(HttpClient.class, server.getURL());
+    }
+
+    @AfterAll
+    public static void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+        if (client != null) {
+            client.stop();
+        }
+    }
+
+    @Test
+    public void testHealth() throws Exception {
+        HttpRequest request = HttpRequest.GET("/health");
+        String body = client.toBlocking().retrieve(request);
+        assertNotNull(body);
+        assertEquals(
+                body,
+                "{\"status\":\"UP\"}"
+        );
+    }
+}
